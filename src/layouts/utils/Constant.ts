1e8fdd7801568e57203df6d86d78c60d466875c7
TommyH <hiept81331@gmail.com>
2026-05-11 10:43:19 +0700

    fix fe

--- src/layouts/utils/Constant.ts
+++ src/layouts/utils/Constant.ts
@@ -1,3 +1,4 @@
 // export const endpointBE: string = "http://localhost:5000";
 export const endpointBE: string = "https://api.minhhiep2534.id.vn";
 // export const endpointBE: string = "https://666d5c8ba84b4a56799dd7ae75760df6.serveo.net";
+// fix fe 
